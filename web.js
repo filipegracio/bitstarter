1cc83aca7d541973b78dba1a7b3e1a94efbd6a66
Ubuntu <ubuntu@ip-172-31-44-122.us-west-2.compute.internal>
2013-07-05 23:11:09 +0000

    lksf

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var buff = fs.readFileSync("/home/ubuntu/bitstarter/index.html")
+
+var str = Buffer.toString(buff)
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
